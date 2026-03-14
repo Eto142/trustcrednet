9ec2153628b6f6a511ad6db7cf22646c92251c39
eto <eto.saviour80@gmail.com>
2026-03-14 16:30:54 +0000

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,15 +8,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $featured = Website::query()
-            ->where('is_active', true)
-            ->with('user:id,name,logo_path')
-            ->withCount('approvedTestimonials')
-            ->withAvg('approvedTestimonials', 'rating')
-            ->orderByDesc('approved_testimonials_count')
-            ->limit(8)
-            ->get(['id', 'user_id', 'name', 'slug', 'description']);
-
-        return view('home.homepage', compact('featured'));
+        return view('home.homepage');
     }
 }
