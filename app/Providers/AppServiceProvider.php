681088f0b59a462f246e85463dc5d244807c0a87
eto <eto.saviour80@gmail.com>
2026-03-15 20:44:47 +0000

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,11 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Mail;
+use Symfony\Component\Mailer\Transport\Dsn;
+use Symfony\Component\Mailer\Transport\Smtp\EsmtpTransport;
+use Symfony\Component\Mailer\Transport\Smtp\EsmtpTransportFactory;
+use Symfony\Component\Mailer\Transport\Smtp\Stream\SocketStream;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,5 +28,34 @@ class AppServiceProvider extends ServiceProvider
         $caBundle = 'C:\php-8.2\cacert.pem';
         putenv("CURL_CA_BUNDLE={$caBundle}");
         putenv("SSL_CERT_FILE={$caBundle}");
+
+        // Laravel 11 does not forward stream SSL options from config/mail.php
+        // to Symfony Mailer. We override the smtp driver to patch SSL options directly.
+        Mail::extend('smtp', function (array $config): EsmtpTransport {
+            $factory = new EsmtpTransportFactory();
+            $scheme  = $config['scheme'] ?? (($config['port'] ?? 25) == 465 ? 'smtps' : 'smtp');
+
+            $transport = $factory->create(new Dsn(
+                $scheme,
+                $config['host'] ?? '127.0.0.1',
+                $config['username'] ?? null,
+                $config['password'] ?? null,
+                $config['port'] ?? null,
+                $config
+            ));
+
+            $stream = $transport->getStream();
+            if ($stream instanceof SocketStream) {
+                $stream->setStreamOptions([
+                    'ssl' => [
+                        'verify_peer'      => false,
+                        'verify_peer_name' => false,
+                        'allow_self_signed' => true,
+                    ],
+                ]);
+            }
+
+            return $transport;
+        });
     }
 }
